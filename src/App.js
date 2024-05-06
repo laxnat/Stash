b67694d497ee62a9d3e7d37b399976729950f05b
Axl Tan <98619443+laxnat@users.noreply.github.com>
2024-05-05 22:03:24 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Auth from './components/Auth';
+
+function App() {
+  return (
+    <div>
+      <h1>File Organization App</h1>
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
